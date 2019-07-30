27a27e1677706271357587ed7e65f7ae7f1c13a3
Chris Carlson <chris-carlson-acom@outlook.com>
2019-07-29 21:30:51 -0700

    adds menu buttons

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import headerStyles from "../styles/components/header.module.less";
 
@@ -7,6 +7,31 @@ const ListLink = props => (
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
+
+class MenuButton extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            expanded: false
+        }
+    }
+    render() {
+        return (
+            <div>
+                <button className={headerStyles.menuButton} 
+                    onClick={ () => this.setState({expanded: !this.state.expanded}) }
+                    style={this.state.expanded ? {display: "none"} : null}>
+                    &#9776;
+                </button>
+                <button className={headerStyles.menuButton} 
+                    onClick={ () => this.setState({expanded: !this.state.expanded}) }
+                    style={!this.state.expanded ? {display: "none"} : null}>
+                    &times;
+                </button>
+            </div>
+        )
+    }
+}
 
 export default () => (
     <header className={headerStyles.header}>
@@ -21,6 +46,7 @@ export default () => (
                 <ListLink to="/shows/">Shows</ListLink>
                 <ListLink to="/contact/">Contact</ListLink>
             </ul>
+            <MenuButton />
         </div>
     </header>
 )
